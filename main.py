b94305e930d9595e2ade4492fcb70b24dbf851cc
taifur-rahaman <nmlsDv@gmail.com>
2025-11-13 20:55:04 +0600

    automation todo added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-user_prompt = "Enter a Todo: "
-input_todo = input(user_prompt)
-print(input_todo)
+user_promt = "Enter a Todo: "
+
+todos = []
+
+while True:
+    choice = input("Do you want to add a todo? (y/n): ")
+    if choice.lower() != 'y':
+        break
+    else: 
+        todo = input(user_promt)
+        todos.append(todo)
+        print(todos)
+    
+print(f"You have {len(todos)} todos in the list.")
