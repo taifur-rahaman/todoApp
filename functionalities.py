1c52bae13d7de4f394580f5a290e70cfca6f2c58
taifur-rahaman <nmlsDv@gmail.com>
2025-11-22 18:21:56 +0600

    txt file added and delete todo added

--- functionalities.py
+++ functionalities.py
@@ -1,10 +1,27 @@
-def add_todo(todos):
-    todo = input("Enter a Todo: ")
-    todos.append(todo)
-    print(f"{todo} has been added to your list")
+def add_todo():
+    todo = input("Enter a Todo: ") + "\n"
+    
+    with open("todos.txt", "a") as file:
+        file.write(todo)
+        print("Todo has been added.")
+    
+    return todo
 
 def edit_todo(todos):
     todo_number = int(input("Enter the number which you want to edit: "))
     new_todo = input("Enter the new Todo: ")
     todos[todo_number - 1] = new_todo
     print("Todo has been updated.")
+
+def delete_todo(todos):
+    todo_number = int(input("Enter the number which todo you want to delete: "))
+    todos.pop(todo_number - 1)
+    print("Todo has been deleted.")
+    
+    with open("todos.txt", "w") as file:
+        file.writelines(todos)
+
+
+
+
+
--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import functionalities as func
 
 todos = []
 
+try:
+    with open("todos.txt", "r") as file:
+        todos = file.readlines()
+except FileNotFoundError:
+        todos = []
+
 while True:
-    user_action = input("1. Add Todo\n2. Show Todos\n3. Edit Todos\n4. Exit\nChoose an Action: ")
+    user_action = input("1. Add Todo\n2. Show Todos\n3. Edit Todos\n4. Delete Todos\n5. Exit\nChoose an Action: ")
     
     match user_action:
         case "1":
-            func.add_todo(todos)
+            new_todo = func.add_todo()
+            todos.append(new_todo)
+            
         case "2":
             print("Your Todos:")
             for item in todos:
@@ -18,6 +26,10 @@ while True:
                 print(f"{todos.index(todo) + 1} - {todo}") 
             func.edit_todo(todos)
         case "4":
+            for todo in todos:
+                print(f"{todos.index(todo) + 1} - {todo}") 
+            func.delete_todo(todos)
+        case "5":
             print("Exiting the Application.")
             break
         case _:
